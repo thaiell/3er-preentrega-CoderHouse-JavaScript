2e2cbc7a5554a616d23ea25cdfa13e774885cd93
Thaiel <thaielog@gmail.com>
2023-03-19 02:35:36 -0300

    vamosquevamos

--- main.js
+++ main.js
@@ -2,11 +2,8 @@ let stockStorage = JSON.parse(localStorage.getItem("stockStorage")) || [];
 let shopCartStorage = JSON.parse(localStorage.getItem("shopCartStorage")) || [];
 
 const addProduct = (id) => {
-    console.log(stockStorage);
-    console.log(id);
     let product = stockStorage.find(item => item.idProduct === id);
-    
-    console.log(product);
+ 
     shopCartStorage.push(product);
     localStorage.setItem("shopCartStorage", JSON.stringify(shopCartStorage));
 }
@@ -22,19 +19,23 @@ class Product {
     }
 }
 
+
+// funcion buscada en google para que quede mas bonito el proyecto, sinceridad ante todo
+function toPascalCase(frase) { 
+    return frase.trim().split(' ').map(function(palabra) {
+      return palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+    }).join(' ');
+  }
+
 let formulary = document.getElementById("formulary");
 
 formulary.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    let handmakerName= document.querySelector("#handmakerName").value.toCamelCase();
-    console.log(handmakerName);
+    let handmakerName = toPascalCase(document.querySelector("#handmakerName").value);
     let nameProduct = document.querySelector("#inputName").value.toUpperCase();
-    console.log(nameProduct);
     let priceProduct = document.querySelector("#inputPrice").value;
-    console.log(priceProduct); 
     let totalStock = document.querySelector("#inputTotalStock").value; 
-    console.log(totalStock);
 
     if (nameProduct === "" || handmakerName === "" || isNaN(priceProduct) || isNaN(totalStock)){
     alert("Complete todos los campos")
@@ -84,40 +85,54 @@ cards(stockStorage);
 allFilter.addEventListener("click", () => {
     cardSection.innerHTML = "";
     cards(stockStorage)
-    console.log(stockStorage);
 });
 upToDown.addEventListener("click", () =>{
     cardSection.innerHTML = "";
     let upToDownStorage = stockStorage.slice().sort((a, b) => (a.priceProduct - b.priceProduct));
     cards(upToDownStorage);
-    console.log(upToDownStorage);
 });
 downToUp.addEventListener("click", () => {
     cardSection.innerHTML = "";
     let downToUpStorage = stockStorage.slice().sort((a, b) => (b.priceProduct - a.priceProduct));
     cards(downToUpStorage);
-    console.log(downToUpStorage);
 });
 
-let searchInput = document.getElementById("searchInput");
+ let query = document.getElementById("searchInput").value;
+
+ let searchInput = document.getElementById("searchInput");
+
 let searchButton = document.getElementById("searchButton");
+let searchForm = document.getElementById("searcher");
 
-let query = searchInput.value;
+searchForm.addEventListener("submit", (e) => {
+e.preventDefault();
+    let inputs = e.target.children;
+    let query = inputs[0].value.trim();
+    let arrayIncludes = [];
 
-const searchProducts = (query) => {
+stockStorage.forEach(product => {
+ 
+let includesOrNot = product.nameProduct.toLowerCase().includes(query.toLowerCase());
 
-    stockStorage.filter(function(product) {
-        return product.nameProduct.toLowerCase().includes(query.toLowerCase());
-    });
-}
+const inputValue = query;
+const regex = /\s{2,}/g;
+const newInputValue = inputValue.replace(regex, ' ');
 
 
-searchButton.addEventListener("click", () => {
-    // cardSection.innerHTML = "";
 
-let array = searchProducts(query);
+if(includesOrNot){
+    cardSection.innerHTML = "";
+    arrayIncludes.push(product);
 
-cards(array)
-// cards(array);
-});
+    
 
+    cards(arrayIncludes);
+
+} else if (query === "" || inputValue !== newInputValue){
+      cardSection.innerHTML = "";
+      cards(stockStorage);
+}})
+
+if(arrayIncludes.length > 0){
+    cards(stockStorage);
+}});
