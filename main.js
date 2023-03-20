49493297403dfd67515cd8b5f495d5b56d417646
Thaiel <thaielog@gmail.com>
2023-03-20 16:38:09 -0300

    20del3

--- main.js
+++ main.js
@@ -9,10 +9,12 @@ const addProduct = (id) => {
 }
 
 class Product {
-    constructor(handmakerName, nameProduct, priceProduct, totalStock){
+    constructor(handmakerName, nameProduct, URLimg, URLDescription, priceProduct, totalStock){
         this.idProduct = stockStorage.length > 0 ? Math.max(...stockStorage.map(p => p.idProduct)) + 1 : 1;
-        this.handmakerName = handmakerName
+        this.handmakerName = handmakerName;
         this.nameProduct = nameProduct;
+        this.URLimg = URLimg;
+        this.URLDescription = URLDescription;
         this.priceProduct = priceProduct;
         this.totalStock = totalStock;
         this.totalSales = 0;
@@ -34,21 +36,21 @@ formulary.addEventListener("submit", (e) => {
 
     let handmakerName = toPascalCase(document.querySelector("#handmakerName").value);
     let nameProduct = document.querySelector("#inputName").value.toUpperCase();
+    let URLimg = document.querySelector("#inputURL").value;
+    let URLDescription = document.querySelector("#inputIMGDescription").value;
     let priceProduct = document.querySelector("#inputPrice").value;
     let totalStock = document.querySelector("#inputTotalStock").value; 
 
-    if (nameProduct === "" || handmakerName === "" || isNaN(priceProduct) || isNaN(totalStock)){
+    if (handmakerName === "" || nameProduct === "" || URLimg === "" || URLDescription === "" || priceProduct === "" || totalStock === "" || isNaN(priceProduct) || isNaN(totalStock)){
     alert("Complete todos los campos")
     return;
     }
 
-stockStorage.push(new Product(handmakerName, nameProduct, priceProduct, totalStock));
+stockStorage.push(new Product(handmakerName, nameProduct, URLimg, URLDescription, priceProduct, totalStock));
 
 localStorage.setItem("stockStorage", JSON.stringify(stockStorage));
 
-nameProduct = "";
-priceProduct = "";
-totalStock = "";
+formulary.reset();
 location.reload();
 });
 
@@ -58,20 +60,22 @@ let allFilter = document.getElementById("allFilter");
 let upToDown = document.getElementById("upToDown"); 
 let downToUp = document.getElementById("downToUp"); 
 
+/* CONSTRUCTOR DE CARDS */
+
 const cards = (array) => {
 array.forEach(product => {
         let div = document.createElement("div");
         div.className = "mycard";
     
         div.innerHTML = `
-        <img src="img/collar artesanal 1.jpg" alt="Collar artesanal">
+        <img src="${product.URLimg}" alt="${product.URLDescription}">
         <div class="wordCard">
         <h2 class="productNameCard">${product.nameProduct}</h2>
         <p class="handmakerName">${product.handmakerName}</p>
         </div>
         <div class="buttonToRight">
-            <b>$${product.priceProduct}</b>
-            <button id="addToBuy_${product.idProduct}" class="buttonShopCart">Añadir Al Carrito</button>
+        <b class="price">$${product.priceProduct}</b>
+        <button id="addToBuy_${product.idProduct}" class="buttonShopCart">Añadir Al Carrito</button>
         </div>
         `
         cardSection.append(div);
@@ -119,20 +123,44 @@ const regex = /\s{2,}/g;
 const newInputValue = inputValue.replace(regex, ' ');
 
 
-
-if(includesOrNot){
+ if(includesOrNot){
     cardSection.innerHTML = "";
     arrayIncludes.push(product);
-
-    
-
     cards(arrayIncludes);
-
 } else if (query === "" || inputValue !== newInputValue){
       cardSection.innerHTML = "";
-      cards(stockStorage);
+      cards(stockStorage); 
 }})
-
-if(arrayIncludes.length > 0){
+if(arrayIncludes.length === 0){
     cards(stockStorage);
 }});
+
+
+
+// MODAL
+let shopCartBtn = document.getElementById("shopCartBtn");
+let cardH1 = document.getElementById("h1ShopCart");
+
+shopCartBtn.addEventListener("click", () => {
+    cardH1.innerHTML = "";
+    cardSection.innerHTML = "";
+    let h1 = document.createElement("div");
+    h1.innerHTML = `<h1 class="Cardh1">Carrito De Compras</h1>`
+    
+cardH1.append(h1);
+cards(shopCartStorage);
+
+})
+
+
+
+/* URLS que funcionan
+
+CHICA CON CAMARA : https://images.pexels.com/photos/6307706/pexels-photo-6307706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
+
+COLLAR : https://images.pexels.com/photos/906056/pexels-photo-906056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
+
+SOMBRERO: https://images.pexels.com/photos/984619/pexels-photo-984619.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
+
+RELOJ MANO : https://images.pexels.com/photos/1697214/pexels-photo-1697214.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
+*/
